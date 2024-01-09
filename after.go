f0239c1cbbb51933e8c469f9353ee8615db0930b
Steve Phillips <steve@tryingtobeawesome.com>
2024-01-09 10:55:09 -0800

    Added before.phi after.go go.mod

---
+++ after.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	ch = 5
+)
+
+func main() {
+  fmt.Printf("Hello world channel=%v\n", ch)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/elimisteve/nextgen_parsing
+
+go 1.21.5
